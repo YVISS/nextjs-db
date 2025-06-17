b90c03903d7513edb5067f8c270d2fc2c329b099
Ombac LA <laombac14@gmail.com>
2025-06-17 11:09:23 +0800

    617 1109

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,11 +4,11 @@ import SideNav from "../ui/dashboard/sidenav";
 export default function Layout({ children }: {children: React.ReactNode}){
 
     return (
-        <div className=" flex h-screen flex-col md:flex-row md:overflow-hidden">
-            <div className="w-full flex-none md: w-64">
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+            <div className="w-full flex-none md:w-64">
                 <SideNav />
             </div>
-            <div className="flex-grow_p-6_md:overflow-y-auto_md:p-12">{children}</div>
+            <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
         </div>
 
     );
--- app/ui/font.ts
+++ app/ui/font.ts
@@ -4,4 +4,4 @@ import {Poppins} from 'next/font/google';
 
 export const inter = Inter({subsets: ['latin']});
 export const lusitana = Lusitana({subsets: ['latin'], weight: ["700"]});
-export const poppins = Poppins({subsets: ['latin'], weight: ["800"]});
+export const poppins = Poppins({subsets: ['latin'], weight: ["600"]});
